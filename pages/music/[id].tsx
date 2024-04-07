158d26bb4b677f93629230144d6697248acccea8
mohnapne <kuvaldin2012@gmail.com>
2024-04-08 00:33:03 +0200

    init

--- pages/music/[id].tsx
+++ pages/music/[id].tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 import { motion } from "framer-motion"
 
-import styles from "../../styles/works.module.css"
+import styles from "../../styles/content.module.css"
 import CustomLayout from "../../layouts/customLayout/CustomLayout"
 import { vids } from "../../music"
 import Video from '@/components/Video'
